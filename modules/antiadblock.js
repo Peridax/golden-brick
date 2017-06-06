c8bd418dd53683a7d6e6c713f86592b2e7b68735
Kebabalicious <Kebabalicious@users.noreply.github.com>
2017-06-05 19:26:23 -0500

    Update antiadblock.js

--- modules/antiadblock.js
+++ modules/antiadblock.js
@@ -1,5 +1,8 @@
 if ($('[class="adsbygoogle"]').first().children().length == 0 && forceAdblock) {
-	alert('Disable adblock on brick-hill.com to use the Golden Brick extension!\n\nTo disable adblock on brick-hill navigate to https://www.brick-hill.com and left click the adblock icon in the top right and click "Don\'t run on pages on this domain.".\n\nOnce you\'ve disabled adblock on brick-hill you can navigate to the chrome extension store or go to http://peridax.com/golden-brick to reinstall Golden Brick.');
+
+	alert(["Disable Adblock on Brick-Hill.com to use the Golden Brick extension!",
+	 "To disable Adblock on Brick-Hill, navigate to https://www.brick-hill.com and click on the adblock icon - Click on \"Don't run on pages on this domain.\"",
+         "Once you've disabled Adblock on Brick-Hill, you can go to the Google Chrome store, or http://peridax.com/golden-brick to reinstall Golden Brick."].join("\n\n"));
 
 	chrome.runtime.sendMessage({uninstall: true}, function(response) {
   		console.log('Uninstall request sent');
