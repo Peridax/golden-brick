91569fc9b8a35d0df0a2660020512268d84d6697
Kebabalicious <headrushprogramming@gmail.com>
2017-06-05 19:48:37 -0500

    Rewrote user.js.
    Speedier functionality and direct request to an API that supplies all of the required fields.
    
    Tested and successful.

--- modules/user.js
+++ modules/user.js
@@ -1,30 +1,20 @@
 Core.prototype.user = function() {
-	$.get('https://www.brick-hill.com/money').success(function(data) {
-		if ($('[href*="/user?id="]', data).length) {
-			core.info.username = $('#welcome', data).text().split('Welcome, ').join('');
-			core.info.logged = true;
-			core.info.bucks = parseInt($('[title="bucks"]', data).text());
-			core.info.bits = parseInt($('[title="bits"]', data).text());
-		} else {
-			core.info.logged = false;
-		};
+	$.get(`https://www.brick-hill.com/API/current_user`, function(Response) {
 
-		// Banning users
-		for (i in banned) {
-			if (core.info.username == banned[i]) {
-				uninstall('You are banned from using this extension');
-			};
-		};
+		core.info.logged = false; if (typeof Response == "object" && "ID" in Response) core.info.logged = true;
+		if (core.info.logged) {
+			core.info.bucks = Response.Bucks; core.info.bits = Response.Bits;
+			core.info.username = Response.Username;
+		}
 
-		// Local Storage
-		localStorage.setItem('username', core.info.username);
-		localStorage.setItem('logged', core.info.logged);
-		localStorage.setItem('bucks', core.info.bucks);
-		localStorage.setItem('bits', core.info.bits);
+		for (u in banned) {
+			if (core.info.username == banned[u]) uninstall('You are banned from using this extension.');
+		}
+
+		localStorage.setItem('username', core.info.username); localStorage.setItem('logged', core.info.logged);
+		localStorage.setItem('bucks', core.info.bucks); localStorage.setItem('bits', core.info.bits);
+
 		core.info.lastUpdated = Date.now();
-	});
 
-	// Interval For Updating
-	clearInterval(core.userIntervalWrap);
-	core.userIntervalWrap = setInterval(core.user, core.userInterval);
-};
+	});
+}
