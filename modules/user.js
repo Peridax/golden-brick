308abd8fea79d5551000c31d164f71b2ef69a09f
Peridax <Peridax@outlook.com>
2017-06-07 15:37:04 -0300

    Clean up and minor fixes
    Set the interval to clear outside and before the HTTP request, and placed the ban script inside the if statement when it checks if the user is logged in.

--- modules/user.js
+++ modules/user.js
@@ -1,22 +1,22 @@
 Core.prototype.user = function() {
+	clearInterval(core.userIntervalWrap);
+	
 	$.get(`https://www.brick-hill.com/API/current_user`, function(Response) {
 
 		core.info.logged = false; if (typeof Response == "object" && "ID" in Response) core.info.logged = true;
 		if (core.info.logged) {
 			core.info.bucks = Response.Bucks; core.info.bits = Response.Bits;
 			core.info.username = Response.Username;
-		}
-
-		for (u in banned) {
-			if (core.info.username == banned[u]) uninstall('You are banned from using this extension.');
+			
+			for (u in banned) {
+				if (core.info.username == banned[u]) uninstall('You are banned from using this extension.');
+			}
 		}
 
 		localStorage.setItem('username', core.info.username); localStorage.setItem('logged', core.info.logged);
 		localStorage.setItem('bucks', core.info.bucks); localStorage.setItem('bits', core.info.bits);
 
 		core.info.lastUpdated = Date.now();
-		
-		clearInterval(core.userIntervalWrap);
 		core.userIntervalWrap = setInterval(core.user, core.userInterval);
 
 	});
